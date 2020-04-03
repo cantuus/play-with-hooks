8568ffe0970ed5812c4a9debd06f3599c79bf004
Sophia Koeut <koeutsophia@gmail.com>
2020-04-02 17:55:40 -0700

    added a delete button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import './App.css';
 
-function Todo({ todo, index, completeTodo /*refer to line 54 as it's props being passed*/ }) {
+function Todo({ todo, index, completeTodo, deleteTodo /*refer to line 54 as it's props being passed*/ }) {
   return (
     <div style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }} className="todo">
       {todo.text}
       <div>
         <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => deleteTodo(index)}>x</button>
       </div>
     </div>
   )
@@ -67,12 +68,24 @@ function App() {
     setTodos(newTodos); //set the state
   }
 
+  const deleteTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
 
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            deleteTodo={deleteTodo}
+          />
         ))}
 
         <TodoForm addTodo={addTodo} />
